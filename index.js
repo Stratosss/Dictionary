4353f0044659c64a65703fbb427b3d11e07fc666
Stratos <157527268+Stratosss@users.noreply.github.com>
2024-05-09 12:26:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import axios from "axios"
 const app = express();
 const port =3000;
 
-const api_key = "Type-Key-Here";
+const api_key = "process.env.API_KEY";  //Type API key here
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
